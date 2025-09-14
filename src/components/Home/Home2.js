e65134e1ff2876346e54b84ee24f42226a8f7ee3
BALA <115382856+SigmaBala@users.noreply.github.com>
2025-09-14 19:30:48 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -39,7 +39,7 @@ function Home2() {
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with and
+              with
               <i>
                 <b className="purple">
                   {" "}
