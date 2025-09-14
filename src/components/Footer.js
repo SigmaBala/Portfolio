86cc47fa2c76531a4f847c031d0562dfde62228d
BALA <115382856+SigmaBala@users.noreply.github.com>
2025-09-14 19:04:59 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Bala</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Bala</h3>
+          <h3>Copyright © {year} Balasubramanian</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
