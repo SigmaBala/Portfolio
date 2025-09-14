0d9d7139e0e4f4ddd958e389d0b4f2532c0f3a6e
BALA <115382856+SigmaBala@users.noreply.github.com>
2025-09-14 19:16:18 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
   DiMongodb,
   DiPython,
   DiGithub,
@@ -17,15 +14,6 @@ import { TbBrandGolang } from "react-icons/tb";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
