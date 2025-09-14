88ca2377ff0344af61884b635172e4c02a3ad8e3
BALA <115382856+SigmaBala@users.noreply.github.com>
2025-09-14 19:13:36 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Bala </span>
+            Hi Everyone, I am <span className="purple">Balasubramanian</span>
             from <span className="purple"> Tamilnadu, India.</span>
             <br />
             I am currently studing b.com.
